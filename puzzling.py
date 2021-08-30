c25586c5f437dba05074a4d0113e6952718da5dc
matprocrastinatesverywellforsomeweirdreason <87820743+matprocrastinatesalittletoowell@users.noreply.github.com>
2021-08-30 15:39:04 -0400

    Update puzzling.py

--- puzzling.py
+++ puzzling.py
@@ -1,4 +1,4 @@
-import pygame, random, sqlite3
+import pygame, random, sqlite3, math
 
 
 pygame.init()
@@ -21,6 +21,7 @@ spaceshipx = 185
 spaceshipy = 375
 spaceshipxchange = 0
 
+playerhealth = 3
 playerscore = 0
 
 rocket = pygame.image.load('C:\\rocket.png')
@@ -36,12 +37,20 @@ asteroid1y = 0
 asteroid1health = 1
 asteroid1destroy = False
 asteroid1falling = True
+timesfallen = 1
 
 def shootrocket(x, y):
     screen.blit(rocket, (x, y))
 
 def set_pos(x, y):
     screen.blit(spaceship, (x, y))
+
+def hitasteroid1():
+    distance = math.sqrt((math.pow(asteroid1x-rocketx, 2)) + (math.pow(asteroid1y-rockety, 2)))
+    if distance < 40:
+        return True
+    else:
+        return False
 
 def asteroidpos(x, y):
     screen.blit(asteroid1, (x, y))
@@ -80,13 +89,30 @@ while running:
         spaceshipx = 375
     set_pos(spaceshipx, spaceshipy)
 
-    asteroid1y += 0.5
-    asteroidpos(asteroid1x, asteroid1y)
-    if asteroid1y > 500:
-        playerscore -= 1
-        asteroid1x = random.randint(0, 375)
-        asteroid1y = 0
+    if asteroid1falling == True:
+        asteroid1y += 0.5
+        asteroidpos(asteroid1x, asteroid1y)
 
+        iscollided = hitasteroid1()
+
+        if iscollided == True:
+            asteroid1x = random.randint(0, 375)
+            asteroid1y = 0
+            timesfallen += 1
+            rocketx = spaceshipx
+            firing = False
+            rockety = -1
+            playerscore += 1
+        elif iscollided != True:
+            if asteroid1y > 500:
+                playerhealth -= 1
+                playerscore -= 1
+                asteroid1x = random.randint(0, 375)
+                asteroid1y = 0
+                timesfallen += 1
+            #change when timesfallen is 6
+        print(playerscore)
+        print(playerhealth)
     pygame.display.update()
 conn.commit()
 conn.close()
